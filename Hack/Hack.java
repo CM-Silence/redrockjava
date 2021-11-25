45f8db54240674dd4ec5dc5b5d3fb16cc37d088c
CM-Silence <757902252@qq.com>
2021-11-25 16:01:14 +0800

    Update Hack.java

--- Hack/Hack.java
+++ Hack/Hack.java
@@ -13,13 +13,13 @@ public class Hack {
         /*System.out.println("Hack:是否需要手动输入需要修改的对象类名?\n需要请输入yes,不需要则输入其他内容,系统会使用预设的类名");
         choose = sc.next();
         if(choose.equalsIgnoreCase("yes")){
-            System.out.println("Hack:请输入类名(需要把包名也写上)");
+            System.out.println("hack:请输入类名(需要把包名也写上)");
             className = sc.nextLine();
         }
         else{
-            className = "RedRock_Android_Java.test16.Hero";
+            className = "RedRock_Android_Java.test16.Person";
         }
-        Class<?> hero = Class.forName(className);*/
+        Class<?> person = Class.forName(className);*/
 
         Class<?> person = Class.forName("RedRock_Android_Java.test16.Person"); //找到Person类
         Class<?> getReady = Class.forName("RedRock_Android_Java.test16.GetReady"); //GetReady类
@@ -40,8 +40,12 @@ public class Hack {
         damage.setAccessible(true);
         Field defense = person.getDeclaredField("defense");
         defense.setAccessible(true);
-        Field criticalChance = person.getDeclaredField("criticalChance");
-        criticalChance.setAccessible(true);
+
+        //取得Person类的私有方法(其实我觉得直接改变量会更方便,不过为了记住这个方法还是写着在这吧awa)
+        Method setCriticalChance = person.getDeclaredMethod("setCriticalChance", double.class);
+        setCriticalChance.setAccessible(true);
+        Method getCriticalChance = person.getDeclaredMethod("getCriticalChance");
+        setCriticalChance.setAccessible(true);
 
         outer:do{
             System.out.println("hack:欢迎使用本修改器,请选择你要修改的内容:\n1.角色生命\n2.角色攻击力\n3.角色防御力\n4.角色暴击率\n5.开启一击必杀模式\n6.开始游戏");
@@ -61,13 +65,13 @@ public class Hack {
                     defense.set(obj,judge());
                 }
                 case "4" ->{
-                    System.out.println("hack:当前角色暴击率为:" + criticalChance.get(obj) + "\n请输入你要修改的值:");
-                    criticalChance.set(obj,judge());
+                    System.out.println("hack:当前角色暴击率为:" + getCriticalChance.invoke(obj) + "\n请输入你要修改的值:");
+                    setCriticalChance.invoke(obj,judge());
                 }
                 case "5" ->{
                     System.out.println("hack:已开启一击必杀模式,请开始游戏吧!");
                     damage.set(obj,99999999);
-                    criticalChance.set(obj,99999999);
+                    setCriticalChance.invoke(obj,99999999);
                 }
                 case "6" ->{
                     break outer;
