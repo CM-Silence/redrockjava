fc88fecf3952c73a7a31987d7a884bc830e1bad4
CM-Silence <757902252@qq.com>
2021-11-25 17:31:12 +0800

    test16修改

--- test16/Test16.java
+++ test16/Test16.java
@@ -1,13 +1,13 @@
 package RedRock_Android_Java.test16;
 
 //work4 level4
-//回合制小游戏(回调,面向对象,抽象类练习)
+//回合制小游戏修改版(回调,面向对象,抽象类,反射)
 public class Test16 {
 
     //main方法里面装少点东西应该没问题吧?应该吧?
     public static void main(String[] args) {
         GetReady getReady = new GetReady();
-        getReady.ready();
-        getReady.begin();
+        getReady.ready(); //进行游戏前的准备(让用户输入英雄的属性)
+        getReady.begin(); //开始游戏
     }
 }
